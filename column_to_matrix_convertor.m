a26fac0512865ceda1a1aa89ce675a90ae20de11
Udayansh Khandelwal <101138019+Udayansh@users.noreply.github.com>
2022-03-08 10:41:43 +0530

    Update column_to_matrix_convertor.m

--- column_to_matrix_convertor.m
+++ column_to_matrix_convertor.m
@@ -13,18 +13,6 @@ function output = column_to_matrix_convertor(bit_column, mod_type)
     end
 end
 
-function output = matrix_to_column_convertor(bit_matrix, mod_type)
-    numbits = length(bit_matrix)*mod_type;
-    columns = mod_type;
-    rows = length(bit_matrix);
-    output = zeros(rows*columns, 1);
-
-    for i=1:rows
-        for j=1:columns
-            output((i-1)*columns + j) = bit_matrix(i, j);
-        end
-    end
-end
 
 
 
